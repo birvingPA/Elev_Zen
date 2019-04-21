8057b5faf97b22cc8634494ece900f258aa769b8
Brandon Irving <birving2@Brandons-MacBook-Pro.local>
2019-04-21 11:17:56 -0400

    attempt 2

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,22 +1,30 @@
 import * as React from "react"
 import { HeaderText } from "../../components/common/uicomponents"
-
-const Home = () => (
-  <React.Fragment>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0,
-      }}
-    >
-      <HeaderText
-        text=" Fit for business, fit for life"
-        subText="Go beyond just managing and build a fitness following that lasts. 
-            Glofox software gives you the power to grow your boutique studio."
-      />
-    </div>
-  </React.Fragment>
-)
+class HomeComponent extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        {setTimeout(() => {
+          return (
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `0px 1.0875rem 1.45rem`,
+                paddingTop: 0,
+              }}
+            >
+              <HeaderText
+                text=" Fit for business, fit for life"
+                subText="Go beyond just managing and build a fitness following that lasts. 
+                Glofox software gives you the power to grow your boutique studio."
+              />
+            </div>
+          )
+        }, 300)}
+      </React.Fragment>
+    )
+  }
+}
+const Home = () => <HomeComponent />
 export default Home
