380282fd8203ec6bdee9a98e433ebbc25cf2640f
Brandon Irving <birving2@Brandons-MacBook-Pro.local>
2019-04-21 11:32:06 -0400

    text fix attempt

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,27 +1,33 @@
 import * as React from "react"
 import { HeaderText } from "../../components/common/uicomponents"
 class HomeComponent extends React.Component {
+  state = {
+    isReady: false,
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isReady: true })
+    }, 100)
+  }
   render() {
     return (
       <React.Fragment>
-        {setTimeout(() => {
-          return (
-            <div
-              style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `0px 1.0875rem 1.45rem`,
-                paddingTop: 0,
-              }}
-            >
-              <HeaderText
-                text=" Fit for business, fit for life"
-                subText="Go beyond just managing and build a fitness following that lasts. 
+        {this.state.isReady && (
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            <HeaderText
+              text=" Fit for business, fit for life"
+              subText="Go beyond just managing and build a fitness following that lasts. 
                 Glofox software gives you the power to grow your boutique studio."
-              />
-            </div>
-          )
-        }, 300)}
+            />
+          </div>
+        )}
       </React.Fragment>
     )
   }
