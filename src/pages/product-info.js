f97d45dbf1223616541dcbc03cb055bed73a51a4
Brandon Irving <birving2@Brandons-MacBook-Pro.local>
2019-04-21 04:13:28 -0400

    fixed folder structure to work with netify

--- src/pages/product-info.js
+++ src/pages/product-info.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { ProductPage } from "./components/productPage"
+import { ProductPage } from "../components/productPage"
 
 const SecondPage = () => (
   <Layout>
