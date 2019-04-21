0c2729e816f4e7e2d956da7ec2e375d527737125
Brandon Irving <birving2@Brandons-MacBook-Pro.local>
2019-04-21 10:56:11 -0400

    build fix attempt 6

--- src/pages/productPage/productPage.js
+++ src/pages/productPage/productPage.js
@@ -1,30 +1,27 @@
 import * as React from "react"
 import { HeaderText } from "../../components/common/uicomponents"
 import { Nav, Navbar } from "react-bootstrap"
+const ProductPage = () => (
+  <React.Fragment>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0,
+      }}
+    >
+      <HeaderText
+        text="Products made for your fitness business"
+        subText="Sleek and scalable. Glofox's products are built to make managing your business easy."
+      />
+    </div>
+    <Navbar variant="light" sticky="top">
+      <Nav.Link>Home</Nav.Link>
+      <Nav.Link>Features</Nav.Link>
+      <Nav.Link>Pricing</Nav.Link>
+    </Navbar>
+  </React.Fragment>
+)
 
-export class ProductPage extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <HeaderText
-            text="Products made for your fitness business"
-            subText="Sleek and scalable. Glofox's products are built to make managing your business easy."
-          />
-        </div>
-        <Navbar variant="light" sticky="top">
-          <Nav.Link>Home</Nav.Link>
-          <Nav.Link>Features</Nav.Link>
-          <Nav.Link>Pricing</Nav.Link>
-        </Navbar>
-      </React.Fragment>
-    )
-  }
-}
+export default ProductPage
